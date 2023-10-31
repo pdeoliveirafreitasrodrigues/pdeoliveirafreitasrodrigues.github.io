e14c33fc058ff3ffda0b931a5040d70c3ebdc49f
pdeoliveirafreitasrodrigues <pdeoliveirafreitasrodrigues@gmail.com>
2023-10-30 21:47:23 -0300

    Add Jogos NBA

--- main.js
+++ main.js
@@ -1,56 +1,17 @@
-$(document).ready(function() {
-  // Quando o botão "Abrir Menu" for clicado
-  $("#btnModalMenu").on("click", function() {
-    // Faça uma requisição AJAX para buscar o conteúdo do menu em "menu.html"
-    $.ajax({
-      url: "menu.html",
-      method: "GET",
-      dataType: "html",
-      success: function(data) {
-        // Quando a requisição for bem-sucedida, insira o conteúdo no modal
-        $("#modalContent").html(data);
-        // Exiba o modal
-        $("#modalContainer").show();
-      },
-      error: function() {
-        // Em caso de erro, exiba uma mensagem de erro ou aja de acordo com a situação
-        console.log("Erro ao carregar o menu.");
-      }
-    });
-  });
+import fetch from "fetch";
 
-  // Feche o modal quando o usuário clicar em qualquer lugar fora dele
-  $(document).on("click", function(event) {
-    if ($(event.target).closest("#modalContainer").length === 0) {
-      $("#modalContainer").hide();
-    }
-  });
+const url = "https://api.nba.com/v2/schedule?date=2023-11-01";
 
-  // Quando o botão "Abrir Menu" for clicado
-  $("#openMenuButton").on("click", function() {
-    // Exibe ou oculta o menu ao clicar no botão
-    $("#menuContainer").toggle();
-    // Esconde o conteúdo do currículo quando abrir o menu
-    $("#curriculoContent").hide();
-  });
+const response = await fetch(url);
 
-  // Script para carregar o currículo via AJAX
-  $("#curriculoLink").on("click", function() {
-    $.ajax({
-      url: "src/components/curriculo/curriculo.html",
-      method: "GET",
-      dataType: "html",
-      success: function(data) {
-        // Quando a requisição for bem-sucedida, insira o conteúdo do currículo no modal
-        $("#modalContent").html(data);
-        // Oculta o menu quando carregar o currículo
-        $("#menuContainer").hide();
-        // Exibe o modal
-        $("#modalContainer").show();
-      },
-      error: function() {
-        console.log("Erro ao carregar o currículo.");
-      }
-    });
-  });
-});
+const data = await response.json();
+
+// Exibe os jogos
+for (const game of data["dates"][0]["games"]) {
+  const jogo = document.createElement("div");
+  jogo.textContent = `
+    ${game["homeTeam"]["fullTeamName"]} x ${game["awayTeam"]["fullTeamName"]}
+    ${game["startTime"]}
+  `;
+  document.querySelector("#jogos").appendChild(jogo);
+}
